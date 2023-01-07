65413e255e05705ecb1df9cc295931445c94237c
gabriel-artemio <gabs.artemioferreira@gmail.com.br>
2023-01-07 01:56:05 -0300

    ajustes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,13 +6,6 @@ namespace CoreMVC_SistemaProdutos.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
