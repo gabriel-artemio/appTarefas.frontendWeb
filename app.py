67ce0c5985742bf0a5226a799e438e66e41d9aa9
Gabriel Artemio <43358004+gabriel-artemio@users.noreply.github.com>
2024-05-11 20:29:38 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,95 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+import numpy as np
+import pandas as pd
+import google.generativeai as genai
+import threading
+import time
+from datetime import timedelta
+
+app = Flask(__name__)
+app.secret_key = 'AIzaSyBRvSVWy5zOcZES9KvWg9DCTxnRi9fr4nA'
+
+# Configuração do Google GenAI
+GOOGLE_API_KEY = "AIzaSyBRvSVWy5zOcZES9KvWg9DCTxnRi9fr4nA"
+genai.configure(api_key=GOOGLE_API_KEY)
+
+# Dados
+DOCUMENT1 = {
+    "Titulo": "Corinthians",
+    "Conteúdo": "7 vezes Campeão Brasileiro, 1 vez Campeão Libertadores e 2 vezes Mundial de Clubes, Maior campeão Paulista com 30 títulos"
+}
+
+DOCUMENT2 = {
+    "Titulo": "Santos",
+    "Conteúdo": "8 vezes Campeão Brasileiro, 3 vezes Campeão Libertadores e 2 vezes Mundial de Clubes"
+}
+
+DOCUMENT3 = {
+    "Titulo": "Real Madrid",
+    "Conteúdo": "14 vezes Campeão Champions League e muitas vezes campeões da La Liga"
+}
+
+documents = [DOCUMENT1, DOCUMENT2, DOCUMENT3]
+df = pd.DataFrame(documents)
+df.columns = ['Titulo', 'Conteudo']
+model = "models/embedding-001"
+
+# Função para calcular embeddings
+def embed_fnc(title, text):
+    return genai.embed_content(model=model,
+                               content=text,
+                               title=title,
+                               task_type="RETRIEVAL_DOCUMENT")["embedding"]
+
+df["Embeddings"] = df.apply(lambda row: embed_fnc(row["Titulo"], row["Conteudo"]), axis=1)
+
+# Função para buscar a consulta
+def buscar_consulta(consulta, base, model):
+    embedding_da_consulta = genai.embed_content(model=model,
+                                                content=consulta,
+                                                task_type="RETRIEVAL_QUERY")["embedding"]
+    produtos_escalares = np.dot(np.stack(df["Embeddings"]), embedding_da_consulta)
+    indice = np.argmax(produtos_escalares)
+    return df.iloc[indice]["Conteudo"]
+
+generation_config = {
+    "temperature": 0.5,
+    "candidate_count": 1
+}
+
+def zerar_historico():
+    session.pop('historico', None)
+
+@app.before_request
+def before_request():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=1)
+    session.modified = True
+
+historico = []
+
+# Rota para a página inicial
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if 'historico' not in session:
+        session['historico'] = []
+
+    if request.method == 'POST':
+        consulta = request.form['consulta']
+        trecho = buscar_consulta(consulta, df, model)
+        prompt = f"Reescreva de uma maneira formal: {trecho}"
+        model_2 = genai.GenerativeModel("gemini-1.0-pro", generation_config=generation_config)
+        response = model_2.generate_content(prompt)
+        session['historico'].append((consulta, response.text))
+        return render_template('index.html', historico=session['historico'])
+    
+    return render_template('index.html', historico=session['historico'])
+
+# Rota para limpar o histórico manualmente
+@app.route('/limpar_historico', methods=['POST'])
+def limpar_historico():
+    zerar_historico()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
